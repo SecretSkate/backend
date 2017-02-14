683cf1fe524915789f0fcbb4481c6a0b47729489
Michael Roque <mikeroque1@gmail.com>
2017-02-14 16:44:37 -0700

    Can now upload stuff to S3

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+var dotenv = require('dotenv').config();
+var upload = require('./routes/upload');
 var users = require('./routes/users');
 
 var app = express();
@@ -22,8 +23,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
 app.use('/users', users);
+app.use('/upload', upload);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
